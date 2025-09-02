b7148733c68c47794a9340710f90fc0852eb6a3a
gelee <geleego@gmail.com>
2025-09-02 10:40:25 +0900

    feat(back): STOMP WebSocket JWT 인증 인터셉터 구현
    StompHandler를 통해 CONNECT 시, Authorization 헤더의 JWT 토큰 검증 및 StompWebSocketConfig에 인터셉터 연결

--- chat-back/src/main/java/com/example/chat_back/chat/config/StompWebSocketConfig.java
+++ chat-back/src/main/java/com/example/chat_back/chat/config/StompWebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.example.chat_back.chat.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,9 +10,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final StompHandler stompHandler;
+
+    public StompWebSocketConfig(StompHandler stompHandler) {
+        this.stompHandler = stompHandler;
+    }
+
     // 아래는 WebSocketMessageBrokerConfigurer의 메서드 오버라이딩
 
     @Override
+    // stomp endpoint url 매핑 및 socket 옵션 활성화
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/connect")
                 .setAllowedOrigins("http://localhost:3000") // Securityconfigs와는 별개로 ws CORS 설정 별도 설정
@@ -26,5 +34,12 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
         // '/topic'으로 시작하는 url 형태로 메시지 수신(subscribe)해야 함을 설정
         registry.enableSimpleBroker("/topic");
+    }
+
+    @Override
+    // 웹소켓의 connect, subscribe, disconnect 등의 요청시에는 http 메시지(http header 등)를 넣을 수 있고,
+    // 이를 interceptor가 통해 가로채서 토큰을 검증
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }
